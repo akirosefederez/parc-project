2e01f9a62faf11a31b0fdd93101b6c5531424d7b
akirosefederez <akirosefederez99@gmail.com>
2025-08-29 14:13:10 +0800

    All files, uploaded

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -110,7 +110,7 @@
     </ul>
   </div>
 </div>
-    <script src="main.js"></script>
+    <script src="{{ asset('jsfolder/main.js') }}"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 
     
