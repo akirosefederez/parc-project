91f8ecf81af70fd703d742d78835a5721c2dca71
imrafaelserrano <imrafaelserrano@gmail.com>
2025-09-01 15:40:35 +0800

    Initial Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,66 +4,61 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PARC Foundation</title>
-<!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('cssfolder/main.css') }}">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('cssfolder/main.css') }}">
 
 </head>
-<body><!--checking-->
+<body>
 
-<!-- Logo -->
-    <img class="logo" src="./assets/logo/logo2.png" alt="logo" />   
-<!-- Top Bar -->
-<div class="top-bar">
-  <div class="w-100 d-flex align-items-center justify-content-between px-0">
-    <!-- Left side: Contact info -->
-    <div class="contactlink ps-3">
-      (02) 8350 6350 | program.director@foundation.com.ph
-    </div>
-
-    <!-- Right side: Buttons -->
-    <div class="nav-buttons d-flex">
-      <a href="#" class="btn btn-donate px-3">DONATE</a>
-      <a href="#" class="btn btn-adopt px-3">ADOPT A SCHOLAR</a>
+  <!-- Top Bar -->
+  <div class="top-bar">
+    <div class="w-100 d-flex align-items-center justify-content-between" id="topbar">
+      <div class="contactlink">(02) 8350 6350  |  program.director@foundation.com.ph</div>
+      <div class="d-none d-md-flex mainbuttons">
+        <a href="#" class="btn btn-donate px-3">DONATE</a>
+        <a href="#" class="btn btn-adopt px-3">ADOPT A SCHOLAR</a>
+      </div>
     </div>
   </div>
-</div>
 
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg bg-white shadow-sm">
-  <div class="container">
-    
-    
+  <!-- Logo  -->
+    <img class="logo" src="https://i.ibb.co/spzX6bDX/logo2.png" alt="Logo">
 
-    <!-- Toggler -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg shadow-sm">
+    <div class="container-fluid">
+    <img class="logo1" src="https://i.ibb.co/spzX6bDX/logo2.png" alt="Logo">
 
-    <!-- Menu -->
-    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-      <ul class="navbar-nav mb-2 mb-lg-0">
-        <li class="nav-item"><a href="{{ url('/original') }}" class="nav-link active">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Causes</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pages</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">News</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-      </ul>
-    </div>
+      <!-- Toggler -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <!-- Social Icons -->
-      <div class="social-icons d-none d-lg-flex gap-3 me-3">
+      <!-- Menu -->
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Causes</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Pages</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">News</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+        </ul>
+      </div>
+
+      <!-- Social Icons -->
+      <div class="social-icons d-none d-lg-block me-3">
         <i class="bi bi-facebook"></i>
         <i class="bi bi-linkedin"></i>
         <i class="bi bi-youtube"></i>
         <i class="bi bi-instagram"></i>
       </div>
-  </div>
-</nav>
+    </div>
+  </nav>
 
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
