bfdbace640c58b4c82fa226844bed73dc50727f7
akirosefederez <akirosefederez99@gmail.com>
2025-09-03 09:44:30 +0800

    Fixed Routing for Donate, Navbar, and Welcome Page

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,47 @@
+<!-- Top Bar -->
+  <div class="top-bar">
+    <div class="w-100 d-flex align-items-center justify-content-center" id="topbar">
+      <div class="contactlink">(02) 8350 6350  |  program.director@foundation.com.ph</div>
+    </div>
+  </div>
+
+  <!-- Logo  -->
+            <img class="logo" src="./assets/logo/logo2.png" alt="" />
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg shadow-sm">
+    <div class="container-fluid">
+    <img class="logo1" src="./assets/logo/logo2.png" alt="Logo">
+
+      <!-- Toggler -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Menu -->
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item active"><a href="{{ url('/home') }}" class="nav-link">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Causes</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Pages</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">News</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+        </ul>
+      </div>
+
+      <!-- Social Icons -->
+      <div class="social-icons d-none d-lg-block me-3">
+        <i class="bi bi-facebook"></i>
+        <i class="bi bi-linkedin"></i>
+        <i class="bi bi-youtube"></i>
+        <i class="bi bi-instagram"></i>
+      </div>
+    </div>
+  </nav>
+
+  
+      <div class="d-none d-md-flex mainbuttons">
+        <a href="{{ url('/donate') }}" class="btn btn-donate px-3">DONATE</a>
+        <a href="#" class="btn btn-adopt px-3">ADOPT A SCHOLAR</a>
+      </div>
