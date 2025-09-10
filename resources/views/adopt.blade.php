4c94fe81deedc9d92cc4381a4a91fdc5fc61f0ae
akirosefederez <akirosefederez99@gmail.com>
2025-09-10 10:04:56 +0800

    MInimal Adjustment on the buttons

--- resources/views/adopt.blade.php
+++ resources/views/adopt.blade.php
@@ -101,7 +101,7 @@
             </div>
 
           <!-- One-time / Monthly -->
-          <div class="center-btn lowerbtn">
+          <div class="lowerbtn">
             <a href="#" class="btn1">Platinum</a>
             <a href="#" class="btn2">Share what you can</a>
           </div>
