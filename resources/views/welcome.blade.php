5fedf90bbe3ad9c109e7183a1abc0fc3b235e068
akirosefederez <akirosefederez99@gmail.com>
2025-09-15 09:53:09 +0800

    Added Link Stylesheet for Main Navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('cssfolder/main.css') }}">
+  <link rel="stylesheet" href="{{ asset('cssfolder/mainnavbar.css') }}">
+
 </head>
 <body>
 
