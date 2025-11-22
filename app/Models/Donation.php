778e7d026f7917a280cf3823e733fb7d6f3105d9
akirosefederez <akirosefederez99@gmail.com>
2025-11-22 18:25:23 +0800

    Added new fillable frequency and amount

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -9,22 +9,25 @@ class Donation extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'fname',
-        'lname',
-        'email',
-        'country',
-        'street',
-        'city',
-        'postal',
-        'emailUpdates',
-        'textUpdates',
-        'payment_method',
-        'card_number',
-        'expiration_month',
-        'expiration_year',
-        'cvv',
-        'cover_processing_fee',
-    ];
+protected $fillable = [
+    'fname',
+    'lname',
+    'email',
+    'country',
+    'street',
+    'city',
+    'postal',
+    'emailUpdates',
+    'textUpdates',
+    'payment_method',
+    'card_number',
+    'expiration_month',
+    'expiration_year',
+    'cvv',
+    'cover_processing_fee',
+    'frequency',          // <-- ADD THIS
+    'monthly_amount',     // <-- ADD THIS
+];
+
 }
 
